45c1d5106af17668c6a72d5ea72724827813877e
OwenGraves <ograves.owen@gmail.com>
2016-09-16 19:13:54 -0500

    Initial commit

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+﻿# Load posh-git example profile
+. 'C:\Users\Owen\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+cd 'C:\Users\Owen\Desktop\Scripts'
+
+# Load extra registry drives
+New-PSDrive -PSProvider Registry -Root HKEY_CLASSES_ROOT -Name HKCR > $null
+
+# Create useful aliases
+New-Alias -Name nip -Value New-ItemProperty
+
+function gald {
+Param(
+    [string]$item
+)
+Get-Alias -Definition $item
+}
