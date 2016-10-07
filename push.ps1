4723706eb84d4c9f1f69dd4eedc39ccf3683aa02
OwenGraves <ograves.owen@gmail.com>
2016-10-07 17:49:40 -0500

    Pushed: 10/07/2016 17:49:40

--- push.ps1
+++ push.ps1
@@ -1,4 +1,5 @@
 Copy-Item $env:HOME"\_vimrc" $PSScriptRoot"\Vim" -Force -Confirm
+Push-VimMisc
 #Copy-Item $env:LOCALAPPDATA"\Google\Chrome\User Data\Default\databases\chrome-extension_fjnbnpbmkenffdnngjfgmeleoegfcffe_0" $PSScriptRoot"\Chrome\Stylish" -Recurse -Force -Confirm
 Copy-Item $env:ConEmuDir"\ConEmu.xml" $PSScriptRoot"\ConEmu" -Force -Confirm
 Copy-item $PROFILE $PSScriptRoot"\Powershell" -Force -Confirm
